651301a1b51d726f7632ec1030bf406e8f48c49a
Guilhermee19 <guilherme@noclaftech.com>
2023-04-18 01:08:00 -0300

    feat: alteração logica e menu

--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,32 @@
 import { STATE, create, Client } from "@open-wa/wa-automate";
 import options from "./config/options";
-import { createStick, viaCep } from "./comands";
+import { createStick, messageError, viaCep } from "./comands";
 
 const defaultMessage = `Ola Senhor, \nComo posso te ajudar? 🖥️\n
 *!cep <CEP>* -> ViaCep
 *!fig* -> Criar Figurinha \n`;
 
 const start = async (client: Client) => {
-  console.log("\x1b[1;32m✓ USING:", process.env.USING, "\x1b[0m");
-  console.log("\x1b[1;32m✓ NUMBER:", await client.getHostNumber(), "\x1b[0m");
-  console.log("\x1b[1;32m[SERVER] Servidor iniciado!\x1b[0m");
 
   client.onStateChanged((state: STATE) => {
     console.log("[Status do cliente]", state);
     if (state === "CONFLICT" || state === "UNLAUNCHED") client.forceRefocus();
   });
 
-  client.onMessage((message) => {
+  client.onMessage((message: any) => {
     const { text, from } = message;
+    // console.log(message)
 
-    // let aux = text.indexOf("!") != -1;
-    // console.log(aux);
-
-    const comand = text.split(' ')[0].replace('!', '');
-    console.log('-->', comand);
-
-    switch (comand) {
-      case 'cep': viaCep(client, message); break;
-      case 'fig': createStick(client, message); break;
-      default: client.sendText(from, defaultMessage);
+    if (message.disappearingModeInitiator === 'chat' && text && text[0] === '#') {
+   
+      const comand = text.split(' ')[0].replace('#', '');
+  
+      switch (comand) {
+        case 'cep': viaCep(client, message); break;
+        case 'fig': createStick(client, message); break;
+        default: messageError(client, message); break;
+      }
     }
-
   });
 
   return client;
@@ -38,5 +34,5 @@ const start = async (client: Client) => {
 
 
 create(options(true, start))
-  .then((client) => start(client))
-  .catch((error) => console.log(error));
+  .then((client: any) => start(client))
+  .catch((error: any) => console.log(error));
--- src/comands.ts
+++ src/comands.ts
@@ -1,12 +1,19 @@
 import { Client, Message } from '@open-wa/wa-automate';
+import { decryptMedia } from '@open-wa/wa-decrypt';
 import axios from 'axios';
+
+export async function sendMenu(client: Client, message: Message) {
+    const { from } = message;
+    const defaultMessage = `*╼╼╼╼╼ Central de Comandos 🖥️ ╼╼╼╼╼*\n\nOpa `+message.chat.contact.name+`! o que deseja:\n\n_#fig_ -> + a imagem para criar a figurinha 📄\n_#cep_ ->  + CEP para buscar  📚\n╼╼╼╼╼╼╼╼╼╼╼╼╼╼╼╼╼╼\n╰╼╼>👋 Criado por Gui.\n`;
+    client.sendText(from, defaultMessage);
+}
 
 export async function viaCep(client: Client, message: Message) {
     let cep = message.body.split("!cep ")[1]
 
-
     client.sendText(message.from, "Pesquisando...");
     client.simulateTyping(message.from, true);
+    client.react(message.id, "✅");
 
     const getAddressByCEP = async (cep: string): Promise<any> => {
 
@@ -28,7 +35,7 @@ export async function viaCep(client: Client, message: Message) {
     return getAddressByCEP(cep)
 }
 
-export async function createStick(client: Client, message: Message) {
+export async function createStick2(client: Client, message: Message) {
     const { from, body } = message;
 
     client.sendText(from, "Pesquisando...");
@@ -44,3 +51,50 @@ export async function createStick(client: Client, message: Message) {
 
     client.simulateTyping(from, false);
 }
+
+export async function createStick(client: Client, message: Message) {
+    const { id, from, isMedia, mimetype , type, quotedMsg } = message;
+
+    const uaOverride = 'WhatsApp/2.2029.4 Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36';
+  
+    if (isMedia && type === 'image') {
+        const mediaData = await decryptMedia(message, uaOverride);
+        const imageBase64 = `data:${mimetype};base64,${mediaData.toString('base64')}`;
+        await client.react(message.id, "✅");
+        await client.sendImageAsSticker(from, imageBase64, { 
+            author: 'Bot do Gui', 
+            pack: 'Minhas Figurinhas' 
+        });
+    } 
+    else if (quotedMsg && quotedMsg.type == 'image') {
+        const mediaData = await decryptMedia(quotedMsg, uaOverride);
+        const imageBase64 = `data:${quotedMsg.mimetype};base64,${mediaData.toString('base64')}`;
+        await client.react(message.id, "✅");
+        await client.sendImageAsSticker(from, imageBase64, { 
+            author: 'Bot do Gui', 
+            pack: 'Minhas Figurinhas' 
+        });
+    } 
+    else if ((mimetype === 'video/mp4' && (message.duration || 31) < 30) || (mimetype === 'image/gif' && (message.duration || 31) < 30)) {
+        const mediaData = await decryptMedia(message, uaOverride);
+        await client.react(message.id, "✅");
+        await client.sendMp4AsSticker(from, `data:${mimetype};base64,${mediaData.toString('base64')}`, undefined,  { 
+            author: 'Bot do Gui', 
+            pack: 'Minhas Figurinhas' 
+        });
+    } 
+    else if ((quotedMsg?.mimetype === 'video/mp4' && (quotedMsg?.duration || 31) < 30) || (quotedMsg?.mimetype === 'image/gif' && (quotedMsg?.duration || 0) < 30)) {
+        const mediaData = await decryptMedia(quotedMsg, uaOverride);
+        await client.react(message.id, "✅");
+        await client.sendMp4AsSticker(from, `data:${quotedMsg.mimetype};base64,${mediaData.toString('base64')}`, undefined,  { 
+            author: 'Bot do Gui', 
+            pack: 'Minhas Figurinhas' 
+        })
+    }
+}
+
+export async function messageError(client: Client, message: Message) {
+    client.react(message.id, "❓");
+    sendMenu(client, message)
+}
+
